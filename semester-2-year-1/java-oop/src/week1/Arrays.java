a674880dd2b284125faad51fab757252beaa76ca
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-07-03 16:12:45 +1000

    Day 6: Finished Arrays

--- semester-2-year-1/java-oop/src/week1/Arrays.java
+++ semester-2-year-1/java-oop/src/week1/Arrays.java
@@ -2,6 +2,23 @@
 // Covers: declare, initialise, loop through, modify 1D arrays
 public class Arrays {
     public static void main(String[] args) {
-        // Add your code here
+
+        // array = a collection of values of the same data type.
+
+        String[] fruits = {"apple", "orange", "banana", "coconut"};
+
+        // fruits[0] = "pineapple";
+        // int numOfFruits = fruits.length;
+
+//        for(int i = 0; i < fruits.length; i ++){
+//            System.out.print(fruits[i] + " ");
+//        }
+        // Arrays.sort(fruits); # Sorts them alphabetically
+        // Arrays.fill(fruits, "pineapple") # Replaces all values in the fruit array with the new variable.
+
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+
     }
 }
