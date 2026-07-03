708250747d8be610986eb255d5a8c067a351f4a8
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-07-03 15:42:22 +1000

    Day 5: Finished for loops.

--- semester-2-year-1/java-oop/src/week1/Loops.java
+++ semester-2-year-1/java-oop/src/week1/Loops.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
+
 // Day 5 — Loops
 // Covers: for loop, while loop, do-while, nested loops
 public class Loops {
-    public static void main(String[] args) {
-        // Add your code here
+    public static void main(String[] args) throws InterruptedException {
+
+        // for Loop = execute some code a CERTAIN amount of times.
+
+//        for(int i = 1; i <= 10; i += 3){
+//            System.out.println(i);
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter a number of times you wish to loop: ");
+//        int max = scanner.nextInt();
+//
+//        for(int i = 1;i < max;i++){
+//            System.out.println(i);
+//        }
+//
+//        scanner.close();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many seconds to count down from: ");
+
+        int start = scanner.nextInt();
+
+        for(int i = start; i > 0; i --){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+
+        System.out.println("HAPPY NEW YEARS!");
     }
 }
