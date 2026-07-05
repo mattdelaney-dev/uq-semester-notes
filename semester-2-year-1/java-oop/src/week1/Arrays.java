bfe2397411522dee9192549210eb6f0c8f39af94
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-07-05 14:02:06 +1000

    Day 8: Finished ArrayList

--- semester-2-year-1/java-oop/src/week1/Arrays.java
+++ semester-2-year-1/java-oop/src/week1/Arrays.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 // Day 6 — Arrays
 // Covers: declare, initialise, loop through, modify 1D arrays
 public class Arrays {
@@ -5,7 +9,7 @@ public class Arrays {
 
         // array = a collection of values of the same data type.
 
-        String[] fruits = {"apple", "orange", "banana", "coconut"};
+//        String[] fruits = {"apple", "orange", "banana", "coconut"};
 
         // fruits[0] = "pineapple";
         // int numOfFruits = fruits.length;
@@ -16,9 +20,49 @@ public class Arrays {
         // Arrays.sort(fruits); # Sorts them alphabetically
         // Arrays.fill(fruits, "pineapple") # Replaces all values in the fruit array with the new variable.
 
-        for(String fruit : fruits){
-            System.out.println(fruit);
+//        for(String fruit : fruits){
+//            System.out.println(fruit);
+//        }
+
+        // ArrayList = A resizeable array that stores objects (autoboxing).
+
+//        ArrayList<String> fruits = new ArrayList<>();
+//
+//        fruits.add("Apple");
+//        fruits.add("Banana");
+//        fruits.add("Coconut");
+
+//        fruits.remove(1);
+//        fruits.set(0, "Pineapple");
+
+
+//        System.out.println(fruits.get(1));
+
+//        System.out.println(fruits.size());
+
+//        Collections.sort(fruits);
+//
+//        for(String fruit : fruits){
+//            System.out.println(fruit);
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> foods = new ArrayList<>();
+
+        System.out.print("Enter the number of food items: ");
+        int numOfFood = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i = 1; i <= numOfFood; i++){
+            System.out.print("Enter food item #" + i + ": ");
+            String food =  scanner.nextLine();
+            foods.add(food);
         }
 
+        Collections.sort(foods);
+        System.out.println(foods);
+
+        scanner.close();
     }
 }
