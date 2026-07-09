ce6d196a842594e4267a4106a792b17254f5dd42
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-07-09 12:18:51 +1000

    Day 10: Finished String Methods

--- semester-2-year-1/java-oop/src/week2/Strings.java
+++ semester-2-year-1/java-oop/src/week2/Strings.java
@@ -2,6 +2,39 @@
 // Covers: length, toUpperCase, substring, contains, replace, equals
 public class Strings {
     public static void main(String[] args) {
-        // Add your code here
+        String name = "Broski";
+
+        int length = name.length();
+        char letter = name.charAt(0);
+        int index = name.indexOf("e");
+        int lastIndex = name.lastIndexOf("t");
+
+        // name = name.toUpperCase();
+        // name = name.toLowerCase();
+        // name = name.trim();
+        // name = name.replace("e", "a");
+
+        // name.isEmpty();
+
+//        if(name.isEmpty()){
+//            System.out.println("Empty String");
+//        }
+//        else{
+//            System.out.println("String");
+//        }
+
+//        if(name.contains(" ")){
+//            System.out.println("your name contains spaces");
+//        }
+//        else{
+//            System.out.println("your name doesnt contain any spaces");
+//        }
+
+        if(name.equalsIgnoreCase("password")){
+            System.out.println("Your name cant be password");
+        }
+        else{
+            System.out.println("Hello " + name);
+        }
     }
 }
