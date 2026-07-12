d7be53e2a6d7598afaaa17f45576a3c89512d52c
Matthew Delaney <matt.delaney.dev@gmail.com>
2026-07-12 13:48:56 +1000

    Day 13: Finished Constructors

---
+++ semester-2-year-1/java-oop/src/week2/Constructor.java
@@ -0,0 +1,10 @@
+public class Constructor {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Bob", 30, 3.2);
+        Student student2 = new Student("bob2", 10, 4.2);
+
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
